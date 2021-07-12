9b219eb1885fde169698d6235139cec21239392b
Jang Kichang <windykc@gmail.com>
2021-07-12 11:28:22 +0900

    [2021.7.3-RELEASE] version 확인 기능, 실패로그 쓰는 버그 수정

--- src/main/java/com/kichang/syslog/SysloggerApplication.java
+++ src/main/java/com/kichang/syslog/SysloggerApplication.java
@@ -1,7 +1,10 @@
 package com.kichang.syslog;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,10 +16,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.integration.channel.NullChannel;
 import org.springframework.integration.ip.udp.UnicastReceivingChannelAdapter;
 import org.springframework.integration.syslog.RFC5424MessageConverter;
 import org.springframework.integration.syslog.SyslogHeaders;
 import org.springframework.integration.syslog.inbound.UdpSyslogReceivingChannelAdapter;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -33,9 +38,24 @@ public class SysloggerApplication {
 	Log logger = LogFactory.getLog(SysloggerApplication.class);
 	@Autowired PropertyService propertyService;
 	@Autowired Environment env;
-	
-	
+
 	public static void main(String[] args) {
+	    for(String arg : args) {
+	        if (arg.equals("-v") || arg.equals("--version")) {
+	          InputStream is = SysloggerApplication.class.getClassLoader().getResourceAsStream("version.txt");
+	          Properties prop = new Properties();
+	          try {
+	            prop.load(is);
+	            String version = (String)prop.get("Version");
+	            System.out.println(version);
+	            System.exit(1);
+	          } catch (IOException e) {
+	            System.err.println("version.txt not found in classpath");
+	            System.exit(1);
+	          }
+	        }
+	      }
+	      
 		//SpringApplication.run(SysloggerApplication.class, args);
 		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(SysloggerApplication.class)
 		        .properties("spring.config.location=classpath:/application.yml," + 
@@ -56,48 +76,26 @@ public class SysloggerApplication {
 		//Log logger = LogFactory.getLog("udpReceiver");
 	    final UdpSyslogReceivingChannelAdapter adapter = new UdpSyslogReceivingChannelAdapter();
 	    adapter.setUdpAdapter(receiver());
-	    adapter.setOutputChannel( (message, timeout) -> { 
-	    	Map<String,?> map = (Map)message.getPayload();
-	    	String msg = (String)map.get(SyslogHeaders.UNDECODED);
-	    	boolean ret = false;
-	    	try {
-	    		logger.info("output channel : " + msg);
-	    		
-	    		
-	    		List<String[]> list = propertyService.getListConfig();	
-				for(String[] parts : list) {
-					SyslogMessageSender sender = getMessageSender(parts);
-					//System.out.println(String.format("send to : %s:%s:%s", parts[0], parts[1], parts[2]));
-					sender.sendMessage(msg);
-				}
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			} 
-	    	return ret;
-	    });
+	    adapter.setOutputChannel((message, timeout) -> {
+	    			Map<String,?> map = (Map)message.getPayload();
+	    	    	String msg = (String)map.get(SyslogHeaders.UNDECODED);
+	    	    	try {
+	    	    		logger.debug("output channel msg : " + msg);
+	    	    		List<String[]> list = propertyService.getListConfig();	
+	    				for(String[] parts : list) {
+	    					SyslogMessageSender sender = getMessageSender(parts);
+	    					logger.debug(String.format("send to : %s:%s:%s", parts[0], parts[1], parts[2]));
+	    					sender.sendMessage(msg);
+	    				}
+	    			} catch (Exception e) {
+	    				logger.error(e.getMessage());
+	    			} 
+	    	    	return true;
+	    		});
 	    adapter.setConverter(new RFC5424MessageConverter());
 	    return adapter;
 	}
 
-	@Bean
-	public UnicastReceivingChannelAdapter receiver() {
-		int port = 514;
-		String p = env.getProperty("syslog.port");
-		if (p != null && org.apache.commons.lang3.StringUtils.isNumeric(p)) {
-			port = Integer.parseInt(p);
-		}
-		logger.info("Syslogger is listen on udp port " + port);
-	    UnicastReceivingChannelAdapter adapter = new UnicastReceivingChannelAdapter(port);
-	    adapter.setTaskExecutor(executor());
-	    return adapter;
-	}
-
-	@Bean
-	public TaskExecutor executor() {
-	    ThreadPoolTaskExecutor exec = new ThreadPoolTaskExecutor();
-	    exec.setCorePoolSize(5);
-	    return exec;
-	}
 	
 	private SyslogMessageSender getMessageSender(String pars[]) throws InvalidateConfigFormatException {
 		if (pars[0].equalsIgnoreCase("tcp")) {
@@ -109,6 +107,7 @@ public class SysloggerApplication {
 		}
 	}
 	
+
 	private TcpSyslogMessageSender getTcpSyslogMessageSender(String ip, String port) {
 		TcpSyslogMessageSender messageSender = new TcpSyslogMessageSender();
 		messageSender.setDefaultMessageHostname("shellguard"); // some syslog cloud services may use this field to transmit a secret key
@@ -133,5 +132,29 @@ public class SysloggerApplication {
 		
 		return messageSender;
 	}
+	
+	
+	@Bean
+	public UnicastReceivingChannelAdapter receiver() {
+		int port = 514;
+		String p = env.getProperty("syslog.port");
+		if (p != null && org.apache.commons.lang3.StringUtils.isNumeric(p)) {
+			port = Integer.parseInt(p);
+		}
+		logger.info("Syslogger is listen on udp port " + port);
+	    UnicastReceivingChannelAdapter adapter = new UnicastReceivingChannelAdapter(port);
+	    adapter.setTaskExecutor(executor());
+	    return adapter;
+	}
+
+	@Bean
+	public TaskExecutor executor() {
+	    ThreadPoolTaskExecutor exec = new ThreadPoolTaskExecutor();
+	    exec.setCorePoolSize(5);
+	    return exec;
+	}
+	
+
+	
 
 }
